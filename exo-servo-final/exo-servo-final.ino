ae6c23c26bca56ca6bf1405f16847bd75e29d1c7
cantyoudobetter <bordelon@10xscale.com>
2018-09-18 09:27:55 -0500

    cleanup

--- exo-servo-final/exo-servo-final.ino
+++ exo-servo-final/exo-servo-final.ino
@@ -3,39 +3,34 @@ exocet supercharger bypass controller
 
 */
 
-// constants won't change. They're used here to set pin numbers:
-const int buttonPin = 7;     // the number of the pushbutton pin
+const int inputPin = 7;     // input pin from Megasquirt
 const int ledPin =  13;      // the number of the LED pin
 
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int throttleState = 0;         // variable for reading the pushbutton status
+const int angleOff = 150;
+const int angleOn = 40;
 
 void setup() {
-  // initialize the LED pin as an output:
   myservo.attach(2); 
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(inputPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  throttleState = digitalRead(inputPin);
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
+  if (throttleState == HIGH) {
     digitalWrite(ledPin, HIGH);
-    myservo.write(150);              // tell servo to go to position in variable 'pos'
+    myservo.write(angleOff);              
     delay(100);
   } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
-    myservo.write(40);              // tell servo to go to position in variable 'pos'
+    myservo.write(angleOn);              
     delay(100);
   }
 }
