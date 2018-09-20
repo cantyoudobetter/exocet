75aae40c8d524cead854f9bf33aa1008824a3d1b
michael.bordelon <bordelon@10xscale.com>
2018-09-20 16:25:57 -0500

    changes

--- exo-servo-final/exo-servo-final.ino
+++ exo-servo-final/exo-servo-final.ino
@@ -4,19 +4,17 @@ exocet supercharger bypass controller
 */
 
 const int inputPin = 7;     // input pin from Megasquirt
-const int ledPin =  13;      // the number of the LED pin
-
+const int servoPin = 2;
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo; 
 
-int throttleState = 0;         // variable for reading the pushbutton status
+int throttleState = 0;         
 const int angleOff = 150;
 const int angleOn = 40;
 
 void setup() {
-  myservo.attach(2); 
-  pinMode(ledPin, OUTPUT);
+  myservo.attach(servoPin); 
   pinMode(inputPin, INPUT_PULLUP);
 }
 
@@ -24,12 +22,9 @@ void loop() {
   throttleState = digitalRead(inputPin);
 
   if (throttleState == HIGH) {
-    digitalWrite(ledPin, HIGH);
     myservo.write(angleOff);              
     delay(100);
   } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
     myservo.write(angleOn);              
     delay(100);
   }
